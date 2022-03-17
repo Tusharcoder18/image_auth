82d042f4d4a456d2d02ba1ffe7eb0fd51189ead2
Tusharcoder18 <ghostridertushar@gmail.com>
2022-03-17 11:31:38 +0530

    Conflicts

--- test/services_test.dart
+++ test/services_test.dart
@@ -1,12 +1,12 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:image_auth/services/image_fetcher_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:image_auth/services/image_fetcher_service.dart';
 
-// void main() {
-//   test("should fetch random image urls", () async {
-//     final _imageFetcher = ImageFetcherService();
+void main() {
+  test("should fetch random image urls", () async {
+    final _imageFetcher = ImageFetcherService();
 
-//     await _imageFetcher.fetchRandomImages();
-//     final _imageUrls = _imageFetcher.imageUrls;
-//     expect(_imageUrls.length, 9);
-//   });
-// }
+    // await _imageFetcher.fetchRandomImages();
+    // final _imageUrls = _imageFetcher.imageUrls;
+    // expect(_imageUrls.length, 9);
+  });
+}
